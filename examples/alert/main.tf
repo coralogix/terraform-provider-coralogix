a69c59dc566391743d84e8f07c8d3bff734e1a27
Or Novogroder <108669655+OrNovo@users.noreply.github.com>
2023-01-03 06:56:22 +0200

    changing arithmetic_operator_modifier to optional and fixing documentation (#57)
    * changing arithmetic_operator_modifier to optional
    
    * adding descriptions and validations to lucene metric-alert 
    
    * fixing field_format_standard description

--- examples/alert/main.tf
+++ examples/alert/main.tf
@@ -206,11 +206,11 @@ resource "coralogix_alert" "metric_lucene_alert" {
       search_query = "name:\"Frontend transactions\""
       condition {
         metric_field                 = "subsystem"
-        arithmetic_operator          = "Avg"
+        arithmetic_operator          = "Percentile"
+        arithmetic_operator_modifier = 20
         less_than                    = true
         group_by                     = ["coralogix.metadata.sdkId"]
         threshold                    = 60
-        arithmetic_operator_modifier = 2
         sample_threshold_percentage  = 50
         time_window                  = "30Min"
         manage_undetected_values {
--- known-issues.md
+++ known-issues.md
@@ -1,2 +1,3 @@
 # Coralogix Provider known issues
 
+* Metric lucene alert - There is no validation when _arithmetic_operator_modifier_ is set and _arithmetic_operator_ is not "Percentile". This validation will be added later.
