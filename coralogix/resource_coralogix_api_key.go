ab03e8c85729961b9dd9d95eba7489cf9afd5422
mbedulli <marco.bedulli@coralogix.com>
2024-02-09 11:27:36 +0100

    PlanModifier

--- coralogix/resource_coralogix_api_key.go
+++ coralogix/resource_coralogix_api_key.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -94,6 +95,9 @@ func (r *ApiKeyResource) Schema(ctx context.Context, req resource.SchemaRequest,
 								path.MatchRelative().AtParent().AtName("user_id"),
 							),
 						},
+						PlanModifiers: []planmodifier.Int64{
+							int64planmodifier.RequiresReplace(),
+						},
 					},
 					"user_id": schema.StringAttribute{
 						Optional: true,
@@ -101,6 +105,9 @@ func (r *ApiKeyResource) Schema(ctx context.Context, req resource.SchemaRequest,
 							stringvalidator.ExactlyOneOf(
 								path.MatchRelative().AtParent().AtName("team_id"),
 							),
+						},
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.RequiresReplace(),
 						},
 					},
 				},
