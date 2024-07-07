117d22f263fb53de6549455bcf8936da6fcdac7b
Or Novogroder <or.novogroder@coralogix.com>
2024-07-07 21:14:19 +0300

    acc tests

--- coralogix/resource_coralogix_alert_test.go
+++ coralogix/resource_coralogix_alert_test.go
@@ -356,6 +356,250 @@ func TestAccCoralogixResourceAlert_logs_more_than_usual_alert(t *testing.T) {
 	)
 }
 
+func TestAccCoralogixResourceAlert_logs_less_than_usual_alert(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckAlertDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCoralogixResourceAlertLogsLessThanUsual(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(alertResourceName, "name", "logs-less-than alert example"),
+					resource.TestCheckResourceAttr(alertResourceName, "description", "Example of logs-less-than alert example from terraform"),
+					resource.TestCheckResourceAttr(alertResourceName, "priority", "P2"),
+					resource.TestCheckResourceAttr(alertResourceName, "labels.alert_type", "security"),
+					resource.TestCheckResourceAttr(alertResourceName, "labels.security_severity", "high"),
+					resource.TestCheckTypeSetElemNestedAttrs(alertResourceName, "notification_group.simple_target_settings.*",
+						map[string]string{
+							"recipients.#": "2",
+							"recipients.0": "example@coralogix.com",
+							"recipients.1": "example2@coralogix.com",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(alertResourceName, "notification_group.simple_target_settings.*",
+						map[string]string{
+							"integration_id": "17730",
+						},
+					),
+					resource.TestCheckResourceAttr(alertResourceName, "incidents_settings.notify_on", "Triggered and Resolved"),
+					resource.TestCheckResourceAttr(alertResourceName, "incidents_settings.retriggering_period.minutes", "1"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.days_of_week.#", "2"),
+					resource.TestCheckTypeSetElemAttr(alertResourceName, "schedule.active_on.days_of_week.*", "Wednesday"),
+					resource.TestCheckTypeSetElemAttr(alertResourceName, "schedule.active_on.days_of_week.*", "Thursday"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.start_time.hours", "10"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.start_time.minutes", "30"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.end_time.hours", "20"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.end_time.minutes", "30"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.threshold", "2"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.time_window.specific_value", "10_MINUTES"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.evaluation_window", "Dynamic"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.lucene_query", "message:\"error\""),
+					resource.TestCheckTypeSetElemNestedAttrs(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.label_filters.application_name.*",
+						map[string]string{
+							"operation": "NOT",
+							"value":     "nginx",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.label_filters.subsystem_name.*",
+						map[string]string{
+							"operation": "AND",
+							"value":     "subsystem-name",
+						},
+					),
+					resource.TestCheckTypeSetElemAttr(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.label_filters.severities.*", "Warning"),
+					resource.TestCheckTypeSetElemAttr(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.label_filters.severities.*", "Error"),
+				),
+			},
+			{
+				ResourceName: alertResourceName,
+				ImportState:  true,
+			},
+			{
+				Config: testAccCoralogixResourceAlertLogsLessThanUsualUpdated(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(alertResourceName, "name", "logs-less-than alert example updated"),
+					resource.TestCheckResourceAttr(alertResourceName, "description", "Example of logs-less-than alert example from terraform updated"),
+					resource.TestCheckResourceAttr(alertResourceName, "priority", "P3"),
+					resource.TestCheckResourceAttr(alertResourceName, "notification_group.simple_target_settings.#", "1"),
+					resource.TestCheckTypeSetElemNestedAttrs(alertResourceName, "notification_group.simple_target_settings.*",
+						map[string]string{
+							"integration_id": "17730",
+						},
+					),
+					resource.TestCheckResourceAttr(alertResourceName, "incidents_settings.notify_on", "Triggered Only"),
+					resource.TestCheckResourceAttr(alertResourceName, "incidents_settings.retriggering_period.minutes", "10"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.days_of_week.#", "2"),
+					resource.TestCheckTypeSetElemAttr(alertResourceName, "schedule.active_on.days_of_week.*", "Monday"),
+					resource.TestCheckTypeSetElemAttr(alertResourceName, "schedule.active_on.days_of_week.*", "Thursday"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.start_time.hours", "8"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.start_time.minutes", "30"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.end_time.hours", "20"),
+					resource.TestCheckResourceAttr(alertResourceName, "schedule.active_on.end_time.minutes", "30"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.threshold", "20"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.time_window.specific_value", "2_HOURS"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.evaluation_window", "Rolling"),
+					resource.TestCheckResourceAttr(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.lucene_query", "message:\"error\""),
+					resource.TestCheckTypeSetElemNestedAttrs(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.label_filters.application_name.*",
+						map[string]string{
+							"operation": "OR",
+							"value":     "nginx",
+						},
+					),
+					resource.TestCheckTypeSetElemNestedAttrs(alertResourceName, "type_definition.logs_less_than.logs_filter.lucene_filter.label_filters.application_name.*",
+						map[string]string{
+							"operation": "NOT",
+							"value":     "application_name",
+						},
+					),
+				),
+			},
+		},
+	},
+	)
+}
+
+func testAccCoralogixResourceAlertLogsLessThanUsual() string {
+	return `resource "coralogix_alert" "test" {
+	  name        = "logs-less-than alert example"
+	  description = "Example of logs-less-than alert example from terraform"
+	  priority    = "P2"
+      
+	  labels = {
+		alert_type        = "security"
+		security_severity = "high"
+		}
+
+	  notification_group = {
+		simple_target_settings = [
+		{
+			recipients = ["example@coralogix.com", "example2@coralogix.com"]
+		},
+		{
+			integration_id = "17730"
+		}
+		]
+	 }
+		
+	  incidents_settings = {
+		notify_on           = "Triggered and Resolved"
+		retriggering_period = {
+			minutes = 1
+		}
+	  }
+
+	  schedule = {
+		active_on = {
+			days_of_week = ["Wednesday", "Thursday"]
+			start_time   = {
+				hours   = 10
+				minutes = 30
+			}
+			end_time = {	
+				hours   = 20
+				minutes = 30
+			}
+		}
+	  }
+
+	  type_definition = {
+		logs_less_than = {
+			threshold   = 2
+			time_window = {
+				specific_value = "10_MINUTES"	
+			}
+			evaluation_window = "Dynamic"
+			logs_filter       = {
+				lucene_filter = {
+					lucene_query  = "message:\"error\""
+					label_filters = {
+						application_name = [
+							{
+								operation = "NOT"
+								value     = "application_name"
+							}	
+						]	
+						subsystem_name = [			
+							{
+								operation = "AND"
+								value     = "subsystem-name"		
+							}
+						]
+						severities = ["Warning", "Error"]
+					}
+				}
+			}
+		}
+	  }
+	}
+	`
+}
+
+func testAccCoralogixResourceAlertLogsLessThanUsualUpdated() string {
+	return `resource "coralogix_alert" "test" {
+	  name        = "logs-less-than alert example updated"
+	  description = "Example of logs-less-than alert example from terraform updated"
+	  priority    = "P3"
+
+	  notification_group = {
+		simple_target_settings = [
+		{
+			integration_id = "17730"
+		}
+		]
+	  }
+
+	  incidents_settings = {
+		notify_on           = "Triggered Only"
+		retriggering_period = {
+			minutes = 10
+		}
+	  }
+
+	  schedule = {
+		active_on = {
+			days_of_week = ["Monday", "Thursday"]
+			start_time   = {
+				hours   = 8
+				minutes = 30
+			}
+			end_time = {
+				hours   = 20
+				minutes = 30
+			}
+		}
+	  }
+
+	  type_definition = {
+		logs_less_than = {
+			threshold   = 20
+			time_window = {
+				specific_value = "2_HOURS"
+			}
+			evaluation_window = "Rolling"
+			logs_filter       = {
+				lucene_filter = {
+					lucene_query  = "message:\"error\""
+					label_filters = {
+						application_name = [
+							{
+								operation = "OR"
+								value     = "nginx"
+							},
+							{
+								operation = "NOT"
+								value     = "application_name"
+							}
+						]
+					}
+				}
+			}
+		}
+	  }
+	}
+	`
+}
+
 func testAccCheckAlertDestroy(s *terraform.State) error {
 	client := testAccProvider.Meta().(*clientset.ClientSet).Alerts()
 
@@ -865,6 +1109,7 @@ func testAccCoralogixResourceAlertLogsMoreThanUsual() string {
       minimum_threshold = 2
     }
   }
+}
 `
 }
 
