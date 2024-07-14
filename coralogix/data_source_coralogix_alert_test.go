19e4c28b1c41e827e2f96f1b33c600ef86f15aa6
Or Novogroder <or.novogroder@coralogix.com>
2024-07-14 11:55:27 +0300

    test

--- coralogix/data_source_coralogix_alert_test.go
+++ coralogix/data_source_coralogix_alert_test.go
@@ -1,7 +1,29 @@
 package coralogix
 
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
 var alertDataSourceName = "data." + alertResourceName
 
+func TestAccCoralogixDataSourceAlert(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckActionDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCoralogixResourceAlertLogsImmediate() +
+					testAccCoralogixDataSourceAlert_read(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(alertDataSourceName, "name", "logs immediate alert"),
+				),
+			},
+		},
+	})
+}
 func testAccCoralogixDataSourceAlert_read() string {
 	return `data "coralogix_alert" "test" {
 	id = coralogix_alert.test.id
