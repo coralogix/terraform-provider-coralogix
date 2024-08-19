88a25d09f3ddcdd3640b4a32477e1c057e65be9d
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-08-19 16:38:08 +0200

    fix: added region shorthands (#255)

--- coralogix/provider.go
+++ coralogix/provider.go
@@ -40,11 +40,17 @@ import (
 var (
 	envToGrpcUrl = map[string]string{
 		"APAC1":   "ng-api-grpc.app.coralogix.in:443",
+		"AP1":     "ng-api-grpc.app.coralogix.in:443",
 		"APAC2":   "ng-api-grpc.coralogixsg.com:443",
+		"AP2":     "ng-api-grpc.coralogixsg.com:443",
 		"EUROPE1": "ng-api-grpc.coralogix.com:443",
+		"EU1":     "ng-api-grpc.coralogix.com:443",
 		"EUROPE2": "ng-api-grpc.eu2.coralogix.com:443",
+		"EU2":     "ng-api-grpc.eu2.coralogix.com:443",
 		"USA1":    "ng-api-grpc.coralogix.us:443",
+		"US1":     "ng-api-grpc.coralogix.us:443",
 		"USA2":    "ng-api-grpc.cx498.coralogix.com:443",
+		"US2":     "ng-api-grpc.cx498.coralogix.com:443",
 	}
 	validEnvs = getKeysStrings(envToGrpcUrl)
 )
