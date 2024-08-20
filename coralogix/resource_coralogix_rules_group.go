9a0c1b9b7935cc821f6fee661274a206fedd58bc
Or Novogroder <108669655+OrNovo@users.noreply.github.com>
2024-08-20 14:51:33 +0300

    remove regular_expression_validation (#260)

--- coralogix/resource_coralogix_rules_group.go
+++ coralogix/resource_coralogix_rules_group.go
@@ -1,11 +1,11 @@
 // Copyright 2024 Coralogix Ltd.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -463,10 +463,9 @@ func appendDestinationFieldSchema(m map[string]*schema.Schema) map[string]*schem
 
 func appendRegularExpressionSchema(m map[string]*schema.Schema) map[string]*schema.Schema {
 	m["regular_expression"] = &schema.Schema{
-		Type:         schema.TypeString,
-		Required:     true,
-		ValidateFunc: validation.StringIsValidRegExp,
-		Description:  "Regular expiration. More info: https://coralogix.com/blog/regex-101/",
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "Regular expiration. More info: https://coralogix.com/blog/regex-101/",
 	}
 	return m
 }
