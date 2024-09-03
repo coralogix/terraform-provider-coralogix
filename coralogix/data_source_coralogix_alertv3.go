4b265844458046f6e2435b1255d4c27010d0d537
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-09-03 17:49:42 +0200

    feat: alerts v3 schema (WIP)

---
+++ coralogix/data_source_coralogix_alertv3.go
@@ -0,0 +1,92 @@
+package coralogix
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"terraform-provider-coralogix/coralogix/clientset"
+	alerts "terraform-provider-coralogix/coralogix/clientset/grpc/alerts/v3"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+var _ datasource.DataSourceWithConfigure = &AlertDataSource{}
+
+func NewAlertDataSource() datasource.DataSource {
+	return &AlertDataSource{}
+}
+
+type AlertDataSource struct {
+	client *clientset.AlertsClient
+}
+
+func (d *AlertDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_alertv3"
+}
+
+func (d *AlertDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	clientSet, ok := req.ProviderData.(*clientset.ClientSet)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *clientset.ClientSet, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	d.client = clientSet.Alerts()
+}
+
+func (d *AlertDataSource) Schema(ctx context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	var r AlertV3Resource
+	var resourceResp resource.SchemaResponse
+	r.Schema(ctx, resource.SchemaRequest{}, &resourceResp)
+
+	resp.Schema = frameworkDatasourceSchemaFromFrameworkResourceSchema(resourceResp.Schema)
+}
+
+func (d *AlertDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data *AlertV3ResourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	//Get refreshed Alert value from Coralogix
+	id := data.ID.ValueString()
+	log.Printf("[INFO] Reading Alert: %s", id)
+	getAlertReq := &alerts.GetAlertDefRequest{Id: wrapperspb.String(id)}
+	getAlertResp, err := d.client.GetAlert(ctx, getAlertReq)
+	if err != nil {
+		log.Printf("[ERROR] Received error: %s", err.Error())
+		if status.Code(err) == codes.NotFound {
+			resp.Diagnostics.AddWarning(err.Error(),
+				fmt.Sprintf("Alert %q is in state, but no longer exists in Coralogix backend", id))
+		} else {
+			resp.Diagnostics.AddError(
+				"Error reading Alert",
+				formatRpcErrors(err, getAlertURL, protojson.Format(getAlertReq)),
+			)
+		}
+		return
+	}
+	log.Printf("[INFO] Received Alert: %s", protojson.Format(getAlertResp))
+
+	data, diags := flattenAlert(ctx, getAlertResp.GetAlertDef())
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
