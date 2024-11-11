e31542c55b5a0920a715b5a20e67fe10f5c2db6f
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-11-11 15:26:30 +0100

    fix: allow default parameters in the state

--- coralogix/resource_coralogix_integration.go
+++ coralogix/resource_coralogix_integration.go
@@ -105,6 +105,7 @@ func (r *IntegrationResource) Schema(ctx context.Context, req resource.SchemaReq
 			},
 			"parameters": schema.DynamicAttribute{
 				Required:            true,
+				Computed:            true,
 				MarkdownDescription: "Data required for the integration.",
 			},
 		},
