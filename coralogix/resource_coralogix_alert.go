fc83e0b731cf7a00b6955cd90946069c2f0a2d15
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-02-06 10:15:30 +0100

    fix: added phantom_mode to create call

--- coralogix/resource_coralogix_alert.go
+++ coralogix/resource_coralogix_alert.go
@@ -1725,6 +1725,7 @@ func extractAlertProperties(ctx context.Context, plan *AlertResourceModel) (*cxs
 		IncidentsSettings: incidentsSettings,
 		NotificationGroup: notificationGroup,
 		EntityLabels:      labels,
+		PhantomMode:       typeBoolToWrapperspbBool(plan.PhantomMode),
 		// Schedule is set in the next step
 	}
 
