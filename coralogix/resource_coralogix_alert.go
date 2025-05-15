34672c23f1eb7ba687682063d548ebbad729fdbc
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-05-15 11:12:13 +0200

    fix: go mod

--- coralogix/resource_coralogix_alert.go
+++ coralogix/resource_coralogix_alert.go
@@ -1493,7 +1493,6 @@ func (r *AlertResource) Schema(_ context.Context, _ resource.SchemaRequest, resp
 			},
 			"labels": schema.MapAttribute{
 				Optional:    true,
-				Computed:    true,
 				ElementType: types.StringType,
 			},
 		},
