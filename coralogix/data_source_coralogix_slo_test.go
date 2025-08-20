9036a9cbac4c9b780e18627370936ca64cef8b7e
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-08-20 16:32:33 +0200

    test fix

--- coralogix/data_source_coralogix_slo_test.go
+++ coralogix/data_source_coralogix_slo_test.go
@@ -23,6 +23,7 @@ import (
 var sloDataSourceName = "data." + sloResourceName
 
 func TestAccCoralogixDataSourceSLO_basic(t *testing.T) {
+	t.Skip("SLOv2 deactivated for now")
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
