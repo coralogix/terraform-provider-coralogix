ef3e3fc2de33f519cd81467f1f892b58ee3531fa
lootag <51933845+lootag@users.noreply.github.com>
2025-09-03 11:19:40 +0200

    Recreate hosted dashboard when not found in backend (#417)

--- coralogix/resource_hosted_dashboard.go
+++ coralogix/resource_hosted_dashboard.go
@@ -1,11 +1,11 @@
 // Copyright 2024 Coralogix Ltd.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +29,8 @@ import (
 	gapi "github.com/grafana/grafana-api-golang-client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var (
@@ -208,7 +210,7 @@ func resourceGrafanaDashboardRead(ctx context.Context, d *schema.ResourceData, m
 
 	var diags diag.Diagnostics
 	if err != nil {
-		if strings.HasPrefix(err.Error(), "status: 404") {
+		if status.Code(err) == codes.NotFound {
 			diags = append(diags, diag.Diagnostic{
 				Severity: diag.Warning,
 				Summary:  fmt.Sprintf("Dashboard %q is in state, but no longer exists in grafana", uid),
