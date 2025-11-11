197a1cc455cfab5627df66aad960c96f761c80c7
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-11-11 16:23:03 +0100

    docs updates

--- examples/resources/coralogix_alert/resource.tf
+++ examples/resources/coralogix_alert/resource.tf
@@ -172,9 +172,9 @@ resource "coralogix_alert" "test" {
 # resource "coralogix_global_router" "example" {
 #   name        = "global router example"
 #   description = "global router example"
-#   entity_type = "alerts"
 #   rules = [
 #     {
+#       entity_type = "alerts"
 #       name      = "rule-name"
 #       condition = "alertDef.priority == \"P1\""
 #       targets = [
