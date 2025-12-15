c24cc648aef012ea174347a94fa0c75d6b37c481
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-12-15 15:29:09 +0100

    slo test fix

--- internal/provider/resource_coralogix_slo_v2_test.go
+++ internal/provider/resource_coralogix_slo_v2_test.go
@@ -94,10 +94,10 @@ resource "coralogix_slo_v2" "test" {
   sli = {
     request_based_metric_sli = {
       good_events = {
-        query = "avg(rate(cpu_usage_seconds_total[5m])) by (instance)"
+        query = "avg(rate(cpu_usage_seconds_total[1m])) by (instance)"
       }
       total_events = {
-        query = "avg(rate(cpu_usage_seconds_total[5m])) by (instance)"
+        query = "avg(rate(cpu_usage_seconds_total[1m])) by (instance)"
       }
     }
   }
